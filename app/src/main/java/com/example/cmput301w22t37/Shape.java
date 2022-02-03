1db8250f73c6a8494dd07f943429b6a5b77e560d
ZhiyuanYu133 <77706312+ZhiyuanYu133@users.noreply.github.com>
2022-02-02 22:50:43 -0700

    Update Shape.java

--- app/src/main/java/com/example/cmput301w22t37/Shape.java
+++ app/src/main/java/com/example/cmput301w22t37/Shape.java
@@ -3,7 +3,6 @@ package com.example.cmput301w22t37;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "yellow";
     public Shape(){}
-    private String color = "purple";
+    private String color = "yellow";
 }
